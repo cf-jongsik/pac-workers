6e380f8af2f7d251f136be66cc182fbbaaaa82f3
jjong <jjong84@gmail.com>
2024-07-07 01:48:15 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+function pac(endpoint: string): string {
+	const res = `function FindProxyForURL(url, host) {
+  // No proxy for private (RFC 1918) IP addresses (intranet sites)
+  if (
+    isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+    isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
+    isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0")
+  ) {
+    return "DIRECT";
+  }
+
+  // No proxy for localhost
+  if (isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0")) {
+    return "DIRECT";
+  }
+
+  // Proxy all
+  return "HTTPS ${endpoint}.proxy.cloudflare-gateway.com:443";
+}`;
+	console.log(res);
+	return res;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const endpoint = url.pathname.slice(1).split('.')[0];
+
+		return new Response(pac(endpoint), {
+			headers: {
+				'content-type': 'application/x-ns-proxy-autoconfig',
+			},
+		});
 	},
 } satisfies ExportedHandler<Env>;
